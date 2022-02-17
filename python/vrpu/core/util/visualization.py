7671d28fedb8064a02611af6cd4d8835be367ad8
Andreas Schmidt <moepmoep12@gmail.com>
2022-02-17 18:31:25 +0100

    Improved visualization

--- python/vrpu/core/util/visualization.py
+++ python/vrpu/core/util/visualization.py
@@ -1,7 +1,7 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-from vrpu.core import Graph, Solution, DriveAction, PickUp, Delivery, UTurnGraph
+from vrpu.core import Graph, Solution, DriveAction, PickUp, Delivery, UTurnGraph, TransportAction
 from vrpu.core.graph.search import astar
 
 PATH_COLORS = ["tab:pink", "tab:olive", "tab:cyan", "tab:red", "tab:purple", "tab:orange"]
@@ -42,20 +42,28 @@ def show_solution(solution: Solution, graph: Graph, show_graph_edges=True):
     for tour_idx, tour in enumerate(solution.tours):
         g = nx.DiGraph()
 
+        node_labels = dict()
+        node_labels_order = dict()
+
+        node_labels_order[tour.assigned_vehicle.current_node] = f"  \n  {0}"
+
         # show all nodes
         for node in graph.nodes.values():
-            g.add_node(node.uid, pos=(node.x, node.y), color='tab:blue')
+            g.add_node(node.uid, pos=(node.x, node.y), color='cornflowerblue')
+            node_labels[node.uid] = node.uid
 
+        g.nodes[tour.assigned_vehicle.current_node]["color"] = 'gold'
+
+        # show normal edges of the graph
         if show_graph_edges:
-            # show normal edges of the graph
             for edge, nodes in graph.edges.items():
                 g.add_edge(nodes[0].uid, nodes[1].uid, weight=edge.cost, color='black', width=1, label="")
                 g.add_edge(nodes[1].uid, nodes[0].uid, weight=edge.cost, color='black', width=1, label="")
 
-        # plt.subplot(100 + 10 * len(solution.tours) + tour_idx + 1)
         added_edges = []
         edge_traversals = dict()
         edge_labels = dict()
+        node_counter = 1
 
         for action in tour.actions:
             # color nodes
@@ -64,6 +72,11 @@ def show_solution(solution: Solution, graph: Graph, show_graph_edges=True):
             if isinstance(action, Delivery):
                 g.nodes[action.node]['color'] = 'tab:orange'
 
+            if isinstance(action, TransportAction):
+                node_labels_order[action.node] = f"  \n  {node_counter}"
+                node_counter += 1
+
+        for action in tour.actions:
             if isinstance(action, DriveAction):
                 # show the path taken for each drive action
                 path = get_path(from_node=action.from_node, to_node=action.to_node, graph=graph)
@@ -81,16 +94,31 @@ def show_solution(solution: Solution, graph: Graph, show_graph_edges=True):
                         else:
                             edge_traversals[edge] = [len(added_edges)]
 
-        nx.draw(G=g, pos=nx.get_node_attributes(g, 'pos'), with_labels=True,
+        nx.draw(G=g, pos=nx.get_node_attributes(g, 'pos'), with_labels=False,
                 edge_color=nx.get_edge_attributes(g, 'color').values(),
                 node_color=nx.get_node_attributes(g, 'color').values(),
+                node_size=600,
+                font_size=10,
                 width=list(nx.get_edge_attributes(g, 'width').values()))
+
+        nx.draw_networkx_labels(G=g, pos=nx.get_node_attributes(g, 'pos'),
+                                labels=node_labels,
+                                font_size=10)
+
+        nx.draw_networkx_labels(G=g, pos=nx.get_node_attributes(g, 'pos'),
+                                labels=node_labels_order,
+                                font_size=14,
+                                font_weight="bold",
+                                verticalalignment='top',
+                                horizontalalignment="left")
 
         nx.draw_networkx_edges(G=g, pos=nx.get_node_attributes(g, 'pos'),
                                edgelist=added_edges,
-                               width=8,
-                               alpha=0.5,
-                               edge_color=PATH_COLORS[tour_idx])
+                               width=7,
+                               alpha=0.75,
+                               edge_color="maroon",
+                               arrowsize=15,
+                               arrows=True)
 
         for edge, visits in edge_traversals.items():
             label = f"{visits[0]}"
@@ -99,28 +127,124 @@ def show_solution(solution: Solution, graph: Graph, show_graph_edges=True):
             edge_labels[edge] = label
 
         nx.draw_networkx_edge_labels(G=g, pos=nx.get_node_attributes(g, 'pos'),
-                                     edge_labels=edge_labels)
+                                     edge_labels=edge_labels,
+                                     font_size=8)
 
         plt.show()
+    # for tour_idx, tour in enumerate(solution.tours):
+    #     g = nx.DiGraph()
+    #
+    #     node_labels = dict()
+    #     node_labels_order = dict()
+    #
+    #     # show all nodes
+    #     for node in graph.nodes.values():
+    #         g.add_node(node.uid, pos=(node.x, node.y), color='tab:blue')
+    #         node_labels[node.uid] = node.uid
+    #
+    #     if show_graph_edges:
+    #         # show normal edges of the graph
+    #         for edge, nodes in graph.edges.items():
+    #             g.add_edge(nodes[0].uid, nodes[1].uid, weight=edge.cost, color='black', width=1, label="")
+    #             g.add_edge(nodes[1].uid, nodes[0].uid, weight=edge.cost, color='black', width=1, label="")
+    #
+    #     # plt.subplot(100 + 10 * len(solution.tours) + tour_idx + 1)
+    #     added_edges = []
+    #     edge_traversals = dict()
+    #     edge_labels = dict()
+    #     node_counter = 1
+    #
+    #     for action in tour.actions:
+    #         # color nodes
+    #         if isinstance(action, PickUp):
+    #             g.nodes[action.node]['color'] = 'tab:green'
+    #         if isinstance(action, Delivery):
+    #             g.nodes[action.node]['color'] = 'tab:orange'
+    #
+    #         if isinstance(action, TransportAction):
+    #             node_labels_order[action.node] = f"  \n  {node_counter}"
+    #             node_counter += 1
+    #
+    #     for action in tour.actions:
+    #         if isinstance(action, DriveAction):
+    #             # show the path taken for each drive action
+    #             path = get_path(from_node=action.from_node, to_node=action.to_node, graph=graph)
+    #             if path:
+    #                 for i in range(0, len(path) - 1):
+    #                     from_node = path[i].uid
+    #                     to_node = path[i + 1].uid
+    #                     edge = (from_node, to_node)
+    #                     edge_op = (to_node, from_node)
+    #                     added_edges.append(edge)
+    #                     if edge in edge_traversals:
+    #                         edge_traversals[edge].append(len(added_edges))
+    #                     elif edge_op in edge_traversals:
+    #                         edge_traversals[edge_op].append(len(added_edges))
+    #                     else:
+    #                         edge_traversals[edge] = [len(added_edges)]
+    #
+    #     nx.draw(G=g, pos=nx.get_node_attributes(g, 'pos'), with_labels=False,
+    #             edge_color=nx.get_edge_attributes(g, 'color').values(),
+    #             node_color=nx.get_node_attributes(g, 'color').values(),
+    #             node_size=600,
+    #             font_size=10,
+    #             width=list(nx.get_edge_attributes(g, 'width').values()))
+    #
+    #     nx.draw_networkx_labels(G=g, pos=nx.get_node_attributes(g, 'pos'),
+    #                             labels=node_labels,
+    #                             font_size=10)
+    #
+    #     nx.draw_networkx_labels(G=g, pos=nx.get_node_attributes(g, 'pos'),
+    #                             labels=node_labels_order,
+    #                             font_size=12,
+    #                             font_weight="bold",
+    #                             verticalalignment='top',
+    #                             horizontalalignment="left")
+    #
+    #     nx.draw_networkx_edges(G=g, pos=nx.get_node_attributes(g, 'pos'),
+    #                            edgelist=added_edges,
+    #                            width=8,
+    #                            alpha=0.5,
+    #                            edge_color=PATH_COLORS[tour_idx])
+    #
+    #     for edge, visits in edge_traversals.items():
+    #         label = f"{visits[0]}"
+    #         for i in range(1, len(visits)):
+    #             label += f", {visits[i]}"
+    #         edge_labels[edge] = label
+    #
+    #     nx.draw_networkx_edge_labels(G=g, pos=nx.get_node_attributes(g, 'pos'),
+    #                                  edge_labels=edge_labels)
+    #
+    #     plt.show()
 
 
 def show_uturn_solution(solution: Solution, graph: UTurnGraph, show_graph_edges=True):
-    g = nx.DiGraph()
-
-    # show all nodes
-    for node in graph.base_graph.nodes.values():
-        g.add_node(node.uid, pos=(node.x, node.y), color='tab:blue')
-
-    # show normal edges of the graph
-    for edge, nodes in graph.base_graph.edges.items():
-        g.add_edge(nodes[0].uid, nodes[1].uid, weight=edge.cost, color='black', width=1, label="")
-        g.add_edge(nodes[1].uid, nodes[0].uid, weight=edge.cost, color='black', width=1, label="")
-
     for tour_idx, tour in enumerate(solution.tours):
-        plt.subplot(100 + 10 * len(solution.tours) + tour_idx + 1)
+        g = nx.DiGraph()
+
+        node_labels = dict()
+        node_labels_order = dict()
+
+        node_labels_order[tour.assigned_vehicle.current_node] = f"  \n  {0}"
+
+        # show all nodes
+        for node in graph.base_graph.nodes.values():
+            g.add_node(node.uid, pos=(node.x, node.y), color='cornflowerblue')
+            node_labels[node.uid] = node.uid
+
+        g.nodes[tour.assigned_vehicle.current_node]["color"] = 'gold'
+
+        # show normal edges of the graph
+        if show_graph_edges:
+            for edge, nodes in graph.base_graph.edges.items():
+                g.add_edge(nodes[0].uid, nodes[1].uid, weight=edge.cost, color='black', width=1, label="")
+                g.add_edge(nodes[1].uid, nodes[0].uid, weight=edge.cost, color='black', width=1, label="")
+
         added_edges = []
         edge_traversals = dict()
         edge_labels = dict()
+        node_counter = 1
 
         for action in tour.actions:
             # color nodes
@@ -129,6 +253,11 @@ def show_uturn_solution(solution: Solution, graph: UTurnGraph, show_graph_edges=
             if isinstance(action, Delivery):
                 g.nodes[action.current_node]['color'] = 'tab:orange'
 
+            if isinstance(action, TransportAction):
+                node_labels_order[action.current_node] = f"  \n  {node_counter}"
+                node_counter += 1
+
+        for action in tour.actions:
             if isinstance(action, DriveAction):
                 # show the path taken for each drive action
                 path = get_path(from_node=action.from_node, to_node=action.to_node, graph=graph)
@@ -146,16 +275,31 @@ def show_uturn_solution(solution: Solution, graph: UTurnGraph, show_graph_edges=
                         else:
                             edge_traversals[edge] = [len(added_edges)]
 
-        nx.draw(G=g, pos=nx.get_node_attributes(g, 'pos'), with_labels=True,
+        nx.draw(G=g, pos=nx.get_node_attributes(g, 'pos'), with_labels=False,
                 edge_color=nx.get_edge_attributes(g, 'color').values(),
                 node_color=nx.get_node_attributes(g, 'color').values(),
+                node_size=600,
+                font_size=10,
                 width=list(nx.get_edge_attributes(g, 'width').values()))
+
+        nx.draw_networkx_labels(G=g, pos=nx.get_node_attributes(g, 'pos'),
+                                labels=node_labels,
+                                font_size=10)
+
+        nx.draw_networkx_labels(G=g, pos=nx.get_node_attributes(g, 'pos'),
+                                labels=node_labels_order,
+                                font_size=14,
+                                font_weight="bold",
+                                verticalalignment='top',
+                                horizontalalignment="left")
 
         nx.draw_networkx_edges(G=g, pos=nx.get_node_attributes(g, 'pos'),
                                edgelist=added_edges,
-                               width=8,
-                               alpha=0.5,
-                               edge_color=PATH_COLORS[tour_idx])
+                               width=7,
+                               alpha=0.75,
+                               edge_color="maroon",
+                               arrowsize=15,
+                               arrows=True)
 
         for edge, visits in edge_traversals.items():
             label = f"{visits[0]}"
@@ -164,9 +308,10 @@ def show_uturn_solution(solution: Solution, graph: UTurnGraph, show_graph_edges=
             edge_labels[edge] = label
 
         nx.draw_networkx_edge_labels(G=g, pos=nx.get_node_attributes(g, 'pos'),
-                                     edge_labels=edge_labels)
+                                     edge_labels=edge_labels,
+                                     font_size=8)
 
-    plt.show()
+        plt.show()
 
 
 def get_path(from_node: str, to_node: str, graph: Graph):
